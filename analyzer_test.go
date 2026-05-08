eb19cad4556f711eb098289adf6a4ef15f8498b2
codestutis <kestutisbiskis@gmail.com>
2026-05-08 12:54:02 -0400

    chore: remove random test

--- analyzer_test.go
+++ analyzer_test.go
@@ -4,42 +4,11 @@ import (
 	"fmt"
 	"github.com/corentings/chess/v2"
 	"github.com/corentings/chess/v2/uci"
-	"math/rand"
 	"strings"
 	"testing"
 	"time"
 )
 
-func TestAnalyzer(t *testing.T) {
-
-	// create random game
-	game := chess.NewGame()
-
-	for game.Outcome() == chess.NoOutcome {
-		moves := game.ValidMoves()
-		move := moves[rand.Intn(len(moves))]
-		if err := game.Move(&move, nil); err != nil {
-			panic(err)
-		}
-	}
-
-	// game is made time to analyze
-
-	eng, err := uci.New("stockfish")
-	if err != nil {
-		panic(err)
-	}
-	defer eng.Close()
-
-	a, err := NewAnalyzer(eng)
-
-	if err != nil {
-		panic(err)
-	}
-
-	gameAnalysis, err := a.AnalyzeGame(game)
-	fmt.Println(gameAnalysis.Puzzles)
-}
 
 func TestStockfishGame(t *testing.T) {
 	eng, err := uci.New("stockfish")
